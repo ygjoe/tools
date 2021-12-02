adc7f2de9619e24673a83cf52eb7d36ac7137fc4
yugang <502253898@qq.com>
2021-12-02 16:37:56 +0800

    feat: 格式化文件修改

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-import Link from 'umi/link'
+import Link from 'umi/link';
 
 class App extends React.Component {
-  constructor (props) {
-    super(props)
-
+  constructor(props) {
+    super(props);
   }
 
-  render () {
+  render() {
     return (
       <ul>
         <li>
-          <Link to='/userAgent'>User Agent</Link>
+          <Link to="/userAgent">User Agent</Link>
         </li>
       </ul>
-    )
+    );
   }
 }
 
-export default App
+export default App;
