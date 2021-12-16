34e425389fd6bcbb9392ffab5774dccbb2359b07
yugang <502253898@qq.com>
2021-12-16 11:10:17 +0800

    feat: 新增微信环境状态

--- src/pages/userAgent/index.js
+++ src/pages/userAgent/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import css from './index.less';
+import React from 'react'
+import css from './index.less'
 
 class App extends React.Component {
   constructor (props) {
-    super(props);
+    super(props)
 
   }
 
   render () {
     return (
       <div className={css.main}>
-        {navigator.userAgent}
+        <p>navigator.userAgent = {navigator.userAgent}</p>
+        <p>window.__wxjs_environment = {window.__wxjs_environment}</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
